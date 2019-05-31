ad861d8b1c2f867a7806e272a59cd4a391b79ab4
Denali Balser <denali.balser167@gmail.com>
2019-05-31 13:52:32 +0000

    Automatically backed up by Learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -1,15 +1,15 @@
 class Patient
   attr_reader :name, :appointment, :doctor
 
-  @@all = []
+  #@@all = []
 
-  def self.all
-    @@all
-  end
+  #def self.all
+    #@@all
+#  end
 
   def initialize(name)
     @name = name
-    @@all <<self
+  #  @@all << self
   end
 
   def new_appointment(doctor, date)
