a96dd72bedd69998333a894b44cfd81067eb6a6a
Denali Balser <denali.balser167@gmail.com>
2019-05-31 13:27:14 +0000

    Automatically backed up by Learn

---
+++ lib/patient.rb
@@ -0,0 +1,30 @@
+class Patient
+  attr_reader :name, :appointment, :doctor
+
+  @@all = []
+
+  def self.all
+    @@all
+  end
+
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+
+  def new_appointment(doctor, date)
+    Appointment.new(doctor, self, date)
+  end
+
+  def appointments
+    Appointment.all.select do |appointments|
+      appointments.patient == self
+    end
+  end
+
+  def doctors
+    appointments.collect do |appointment|
+      appointment.doctors
+    end
+  end
+end
